d67a81b82b848218438996a177c7b0d99f55dc3d
YuraDXPNetwork <yurad@xp.network>
2022-11-10 12:10:04 +0200

    connecting pera wallet

--- src/Components/Wallets/Pera.tsx
+++ src/Components/Wallets/Pera.tsx
@@ -1,12 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { HigherALGO } from "./HigherALGO";
 import icon from "../../assets/wallets/Pera.svg";
+import { handleDisconnectPeraWallet, peraWallet } from "./walletConnectors";
 
 interface Props {}
 
-const Pera = () => {
+const Pera = ({
+    styles,
+    connect,
+}: {
+    styles: () => Object;
+    connect: Function;
+}) => {
+    useEffect(() => {
+        peraWallet
+            .reconnectSession()
+            .then((accounts) => {
+                //   peraWallet.connector.on ("disconnect", handleDisconnectPeraWallet);
+
+                if (accounts.length) {
+                }
+            })
+            .catch((e) => console.log(e));
+    }, []);
+
     return (
-        <button>
+        <button style={styles()} onClick={() => connect("Pera")}>
             <img src={icon} alt="" />
             Pera
         </button>
