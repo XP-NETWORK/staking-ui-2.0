91d63b100d7ff7f2b307eda7d181b1d043d159d8
lihishrem <lihishremm@gmail.com>
2022-11-10 15:31:23 +0200

    connected account navbar

--- src/Components/ConnectedAccountNavbar/ActiveAccountNavbar.tsx
+++ src/Components/ConnectedAccountNavbar/ActiveAccountNavbar.tsx
@@ -5,21 +5,23 @@ import Jazzicon from "react-jazzicon";
 // import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
-
-
+import { BLOCKCHAINS } from "../../assets/ts/Consts";
+import classNames from "classnames";
 
 export const ActiveAccountNavbar: FC<{}> = () => {
-    const title="bsc";
+  const chain = BLOCKCHAINS[0];
   const account = "0xe8a049E1CC432bC2eb57331d2cC51e9E2898E0e0";
-
-
 
   const handleSelect = (e: any) => {
     // setValue(e);
   };
 
-//   let title = <div className="nameWrapper typeBtnWidth">{chain} <div className="arrow-down"></div></div>;
-
+  let title = (
+    <div className="ddItem">
+      <img src={chain.img} alt={chain.img} />
+      {chain.chain}
+    </div>
+  );
 
   return (
     <div className="activeWrapper">
@@ -33,21 +35,29 @@ export const ActiveAccountNavbar: FC<{}> = () => {
         NFT Collection
       </Link>
       <div className="chainAndAccountContainer">
+        <DropdownButton
+          onSelect={handleSelect}
+          id="dropdown-basic-button"
+          title={title}
+          size="sm"
+          variant=""
+          className="dropdown"
+        >
+          {BLOCKCHAINS.filter((c) => c.chain !== chain.chain).map((c) => {
+            return (
+              <Dropdown.Item eventKey={c.chain}>
+                <label>
+                  <img src={c.img} alt={c.chain} />
+                  {c.chain}
+                </label>
+              </Dropdown.Item>
+            );
+          })}
+        </DropdownButton>
         <label className="account">
           {/* <Jazzicon diameter={16} address={`${account}`} /> */}
           {account.slice(0, 10) + "..." + account.slice(-2)}
         </label>
-        <DropdownButton
-            onSelect={handleSelect}
-            id="dropdown-basic-button"
-            title={title}
-            size="sm"
-            variant=""
-          >
-            <Dropdown.Item eventKey="Show All">Show All</Dropdown.Item>
-            <Dropdown.Item eventKey="Transfer">Transfer</Dropdown.Item>
-            <Dropdown.Item eventKey="Unfreeze">Unfreeze</Dropdown.Item>
-          </DropdownButton>
       </div>
     </div>
   );
