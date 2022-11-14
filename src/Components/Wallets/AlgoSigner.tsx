c106ac8d182702d612207db2ff8c9f9fe36604e4
lihishrem <lihishremm@gmail.com>
2022-11-14 18:44:43 +0200

    create client

--- src/Components/Wallets/AlgoSigner.tsx
+++ src/Components/Wallets/AlgoSigner.tsx
@@ -34,40 +34,18 @@ const AlgoSigner = ({ connect }: { connect: Function }) => {
     console.log("algosigner", { account });
 
     let client = await createClient(account.signer, account.address);
-    console.log(client);
-
     dispatch(setClient(client));
-    // await client.initialize({
-    //   stakingTokenAddress: account.address,
-    //   communityAddress: communityAddress,
-    //   minClaimPeriod: BigInt(minClaimPeriod),
-    //   rewardRate: BigInt(rewardRate),
-    //   assetId: BigInt(assetIdx),
-    //   duration: BigInt(duration),
-    //   multiplier: BigInt(multiplier),
-    //   maxLoss: BigInt(maxLoss),
-    // });
-    // await client.get_balance_addr();
-    // console.log(await client.getAccountState());
-
-    const axfer = await algosdk.makeAssetTransferTxnWithSuggestedParams(
-      account.address,
-      algosdk.getApplicationAddress(121878733),
-      undefined,
-      undefined,
-      BigInt(10),
-      undefined,
-      assetIdx as any,
-      await algod.getTransactionParams().do()
-    );
-    let alex = await client.stake(axfer, BigInt(1209600) as any);
-    console.log(alex);
+    await client.initialize({
+      stakingTokenAddress: account.address,
+      communityAddress: communityAddress,
+      minClaimPeriod: BigInt(minClaimPeriod),
+      rewardRate: BigInt(rewardRate),
+      assetId: BigInt(assetIdx),
+      duration: BigInt(duration),
+      multiplier: BigInt(multiplier),
+      maxLoss: BigInt(maxLoss),
+    });
   };
-  //  let  balanceAfterStaking1 =  creator_app_client.call(
-  //     Staking.get_balance_addr, foreign_assets=[asset])
-
-  console.log();
-
   return (
     <button onClick={handleClick} className="connectBtn">
       <img style={{ width: "28px" }} src={icon} alt="" />
