fc516b1aa56904118f3987df31359fb5406ec856
lihishrem <lihishremm@gmail.com>
2022-11-14 20:12:08 +0200

    client creation and interact with contract

--- src/Components/Wallets/AlgoSigner.tsx
+++ src/Components/Wallets/AlgoSigner.tsx
@@ -21,9 +21,11 @@ import {
   multiplier,
   rewardRate,
 } from "../../assets/ts/Consts";
+import { useNavigate } from "react-router";
 
 const AlgoSigner = ({ connect }: { connect: Function }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   // const { signer, account } = useSelector(
   //   (state: ReduxState) => state.homePage
   // );
@@ -35,17 +37,55 @@ const AlgoSigner = ({ connect }: { connect: Function }) => {
 
     let client = await createClient(account.signer, account.address);
     dispatch(setClient(client));
-    await client.initialize({
-      stakingTokenAddress: account.address,
-      communityAddress: communityAddress,
-      minClaimPeriod: BigInt(minClaimPeriod),
-      rewardRate: BigInt(rewardRate),
-      assetId: BigInt(assetIdx),
-      duration: BigInt(duration),
-      multiplier: BigInt(multiplier),
-      maxLoss: BigInt(maxLoss),
-    });
+    navigate("/stake");
+    // await client.initialize({
+    //   stakingTokenAddress: account.address,
+    //   communityAddress: communityAddress,
+    //   minClaimPeriod: BigInt(minClaimPeriod),
+    //   rewardRate: BigInt(rewardRate),
+    //   assetId: BigInt(assetIdx),
+    //   duration: BigInt(duration),
+    //   multiplier: BigInt(multiplier),
+    //   maxLoss: BigInt(maxLoss),
+    // });
+    // await client.get_balance_addr();
+    // console.log(await client.getAccountState());
+
+    const axfer: any = algosdk.makeAssetTransferTxnWithSuggestedParams(
+      account.address,
+      algosdk.getApplicationAddress(121878733),
+      undefined,
+      undefined,
+      1,
+      undefined,
+      assetIdx as any,
+      await algod.getTransactionParams().do()
+    );
+    console.log(axfer);
+
+    // let alex = await client.stake({ axfer: axfer, lockTime_: 1209600 });
+    // console.log(alex);
+
+    // let alex1 = await client.getEarned({ address: account.address });
+    // console.log({alex1});
+
+    const assetInfo = await client.client
+      .accountAssetInformation(client.sender, assetIdx)
+      .do();
+    const balance = assetInfo["asset-holding"]["amount"];
+    console.log(balance);
+
+    // let alex2 = await client.get_balance_addr({ appForeignAssets: [assetIdx] });
+    // console.log({ alex2 });
+
+    //console.log("Account state", (await client.getAccountState()).sn); //total staked
   };
+
+  //  let  balanceAfterStaking1 =  creator_app_client.call(
+  //     Staking.get_balance_addr, foreign_assets=[asset])
+
+  console.log();
+
   return (
     <button onClick={handleClick} className="connectBtn">
       <img style={{ width: "28px" }} src={icon} alt="" />
