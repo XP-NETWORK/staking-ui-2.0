d80266ecd0c17b4e15c8cf843c4d089c6f724e7d
lihishrem <lihishremm@gmail.com>
2022-11-17 18:02:29 +0200

    fix algosigner

--- src/assets/ts/algoUtils.ts
+++ src/assets/ts/algoUtils.ts
@@ -6,129 +6,133 @@ import store from "../../store/store";
 export const algod = new algosdk.Algodv2(algodApiKey, algodUri, algodPort);
 
 export const createClient = async (
-    signer: any,
-    account: string,
-    duration: any
+  signer: any,
+  account: string,
+  duration: any
 ) => {
-    console.log("signer,account", signer, account);
-   // debugger;
-    const { connectedWallet } = store.getState().homePage;
-    const stakingContract = new Staking({
-        client: algod,
-        signer: async (txns) => {
-            const s = txns?.map((e) => {
-                return {
-                    txn: Buffer.from(e.toByte()).toString("base64"),
-                };
-            });
-            let signed: any;
-            switch (connectedWallet) {
-                case "Algosigner":
-                    signed = await signer.signTxn(s);
-                    break;
-                case "MyAlgo":
-                    signed = await signer.signTxns(s);
-                    break;
-                case "Pera":
-                    signed = await signer.signTransaction(s);
-                    break;
-                default:
-                    break;
-            }
+  console.log("signer,account", signer, account);
+  // debugger;
+  const { connectedWallet } = store.getState().homePage;
+  console.log({connectedWallet});
 
-            // const signed =
-            //     connectedWallet === "Algosigner"
-            //         ? await signer.signTxn(s)
-            //         : connectedWallet === "MyAlgo"
-            //         ? await signer.signTxns(s)
-            //         : await signer.signTransaction(s);
-            // console.log({ signed });
+  const stakingContract = new Staking({
+    client: algod,
+    signer: async (txns) => {
+      const s = txns?.map((e) => {
+        return {
+          txn: Buffer.from(e.toByte()).toString("base64"),
+        };
+      });
+      let signed: any;
+      switch (connectedWallet) {
+        case "AlgoSigner":
+          console.log("im in");
 
-            return signed?.map((e: any) => Buffer.from(e.blob, "base64"));
-        },
-        sender: account,
-        appId: getAppDetails(duration).id,
-    });
-    return stakingContract;
+          signed = await signer.signTxn(s);
+          break;
+        case "MyAlgo":
+          signed = await signer.signTxns(s);
+          break;
+        case "Pera":
+          signed = await signer.signTransaction(s);
+          break;
+        default:
+          break;
+      }
+
+      // const signed =
+      //     connectedWallet === "Algosigner"
+      //         ? await signer.signTxn(s)
+      //         : connectedWallet === "MyAlgo"
+      //         ? await signer.signTxns(s)
+      //         : await signer.signTransaction(s);
+      // console.log({ signed });
+
+      return signed?.map((e: any) => Buffer.from(e.blob, "base64"));
+    },
+    sender: account,
+    appId: getAppDetails(duration).id,
+  });
+  return stakingContract;
 };
 
 export const stake = async (
-    address: string,
-    amount: number,
-    duration: number,
-    stakingClient: any
+  address: string,
+  amount: number,
+  duration: number,
+  stakingClient: any
 ) => {
-    // console.log(
-    //   "paramns",
-    //   { address },
-    //   { amount },
-    //   { duration },
-    //   { stakingClient }
-    // );
-    // debugger;
-    const axfer: any = algosdk.makeAssetTransferTxnWithSuggestedParams(
-        address,
-        algosdk.getApplicationAddress(stakingClient.appId),
-        undefined,
-        undefined,
-        amount,
-        undefined,
-        assetIdx as any,
-        await algod.getTransactionParams().do()
-    );
-    //console.log(axfer);
+  // console.log(
+  //   "paramns",
+  //   { address },
+  //   { amount },
+  //   { duration },
+  //   { stakingClient }
+  // );
+  // debugger;
+  const axfer: any = algosdk.makeAssetTransferTxnWithSuggestedParams(
+    address,
+    algosdk.getApplicationAddress(stakingClient.appId),
+    undefined,
+    undefined,
+    amount,
+    undefined,
+    assetIdx as any,
+    await algod.getTransactionParams().do()
+  );
+  //console.log(axfer);
 
-    let staking = await stakingClient.stake({
-        axfer: axfer,
-        lockTime_: getAppDetails(duration).duration,
-    });
-    console.log(staking);
+  let staking = await stakingClient.stake({
+    axfer: axfer,
+    lockTime_: getAppDetails(duration).duration,
+  });
+  console.log(staking);
 };
 
 export const unstake = async (
-    address: string,
-    amount: number,
-    duration: number,
-    stakingClient: any
+  address: string,
+  amount: number,
+  duration: number,
+  stakingClient: any
 ) => {
-    // console.log(
-    //   "paramns",
-    //   { address },
-    //   { amount },
-    //   { duration },
-    //   { stakingClient }
-    // );
+  // console.log(
+  //   "paramns",
+  //   { address },
+  //   { amount },
+  //   { duration },
+  //   { stakingClient }
+  // );
 
-    const axfer: any = algosdk.makeAssetTransferTxnWithSuggestedParams(
-        address,
-        algosdk.getApplicationAddress(stakingClient.appId),
-        undefined,
-        undefined,
-        amount,
-        undefined,
-        assetIdx as any,
-        await algod.getTransactionParams().do()
-    );
-    //console.log(axfer);
+  const axfer: any = algosdk.makeAssetTransferTxnWithSuggestedParams(
+    address,
+    algosdk.getApplicationAddress(stakingClient.appId),
+    undefined,
+    undefined,
+    amount,
+    undefined,
+    assetIdx as any,
+    await algod.getTransactionParams().do()
+  );
+  //console.log(axfer);
 
-    let staking = await stakingClient.stake({
-        axfer: axfer,
-        lockTime_: getAppDetails(duration).duration,
-    });
-    // console.log(staking);
+  let staking = await stakingClient.stake({
+    axfer: axfer,
+    lockTime_: getAppDetails(duration).duration,
+  });
+  // console.log(staking);
 };
 
 export const claimXPNET = async () => {};
 
 export const createClients = async (signer: any, account: string) => {
-    // console.log("signer,account create clintsssss", signer, account);
-    let clients = [
-        await createClient(signer, account, 3),
-        await createClient(signer, account, 6),
-        await createClient(signer, account, 9),
-        await createClient(signer, account, 12),
-    ];
-    return clients;
+  // console.log("signer,account create clintsssss", signer, account);
+  let clients = [
+    await createClient(signer, account, 3),
+    await createClient(signer, account, 6),
+    await createClient(signer, account, 9),
+    await createClient(signer, account, 12),
+  ];
+  return clients;
 };
 
 // export const rewardPool = async (signer: any, address: string,stakingClient:any,amount:number) => {
