7bb4e7e89ba526fd4000922a85d7a04e331a14c0
Alex Moshinsky <alexmoshinsky@Alexs-MacBook-Pro-40343.local>
2022-11-19 18:07:49 +0200

    update staking contract

--- src/assets/ts/Consts.ts
+++ src/assets/ts/Consts.ts
@@ -15,22 +15,22 @@ export const algodApiKey =
   "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 export const algodUri = "https://node.testnet.algoexplorerapi.io";
 export const algodPort = 443;
-export const assetIdx = 123148231;
+export const assetIdx = 123937415;
 
 //3 different contracts - every staking periog has her own contract
-export const appAdress3Months = 123148891;
+export const appAdress3Months = 123937177;
 export const duration3Months = 7890000;
 export const multiplier3Months = 130000000000;
 
-export const appAdress6Months = 123149104;
+export const appAdress6Months = 123937248;
 export const duration6Months = 15780000;
 export const multiplier6Months = 150000000000;
 
-export const appAdress9Months = 123149285;
+export const appAdress9Months = 123937297;
 export const duration9Months = 23650000;
 export const multiplier9Months = 170000000000;
 
-export const appAdress12Months = 123149619;
+export const appAdress12Months = 123937342;
 export const duration12Months = 31536000;
 export const multiplier12Months = 190000000000;
 //
@@ -41,7 +41,7 @@ export const multiplier12Months = 190000000000;
 export const minClaimPeriod = 604800;
 export const rewardRate = 1929004;
 export const communityAddress =
-  "G3V2A7GT7RJ3LEK2OTMVYUZT47RZBY4GGNPL4NO6DNBFARGYOJNJTJBJCE"; //my add-lost tokens
+  "CVQFPJPBG4F5XKRHC4LNOTW325NUCFO4SC4K5KYHHVN7YHL3HJWPHODKV4"; //my add-lost tokens
 export const maxLoss = 75000000000;
 
 // export const urlLocNft = "/src/assets/images/nftRewards/";
