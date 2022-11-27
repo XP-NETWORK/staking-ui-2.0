d09d46c5e2e7b53c2a01966ea223b6a6e96849d1
YuraDXPNetwork <yurad@xp.network>
2022-11-27 09:02:36 +0200

    yarn build issue resolved

--- config-overrides.js
+++ config-overrides.js
@@ -6,23 +6,31 @@ module.exports = function override(webpackConfig) {
         stream: require.resolve("stream"),
         buffer: require.resolve("buffer"),
         algosdk: require.resolve("algosdk"),
-        "assert": require.resolve("assert"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify"),
-        "url": require.resolve("url")
-
+        assert: require.resolve("assert"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify"),
+        url: require.resolve("url"),
     };
-
+    webpackConfig.resolve.fallback = webpackConfig;
     webpackConfig.ignoreWarnings = [/Failed to parse source map/];
-
+    webpackConfig.resolve.fallback = {
+        crypto: require.resolve("crypto-browserify"),
+    };
     webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
             process: "process/browser",
             Buffer: ["buffer", "Buffer"],
-
         })
     );
+    webpackConfig.module.rules.push({
+        test: /\.(js|mjs|jsx)$/,
+        enforce: "pre",
+        loader: require.resolve("source-map-loader"),
+        resolve: {
+            fullySpecified: false,
+        },
+    });
 
     return webpackConfig;
 };
